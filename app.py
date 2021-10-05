7eee5270a1f3fabcc0cfab67693bcf6a32ed7bdb
Riya Sharma <45160091+Riyasharma-in@users.noreply.github.com>
2021-10-05 23:56:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ import pandas as pd
 import webbrowser
 # !pip install dash
 import dash
-#import dash_html_components as html
+
 from dash import html
-#import dash_core_components as dcc
+
 from dash import dcc
 import dash_bootstrap_components as dbc
 from matplotlib import pyplot as plt
